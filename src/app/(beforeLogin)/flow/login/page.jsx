3a43e1478ca3870cfc7ac49c782b40406acf1257
KiserLine <hts000203@gmail.com>
2024-05-18 14:21:05 +0900

    update

--- src/app/(beforeLogin)/flow/login/page.jsx
+++ src/app/(beforeLogin)/flow/login/page.jsx
@@ -1,3 +1,46 @@
-export default function loginPage() {
-  return <div>로그인 페이지</div>;
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Login() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <div style={{ border: '2px solid #a7a7a7', borderRadius: '10px', overflow: 'hidden', width: '350px' }}>
+        <div style={{ padding: '20px', textAlign: 'center', fontSize: '20px', color: '#000000' }}>
+          KKMC<br />생산관리 시스템
+        </div>
+        <Image src="/icons/pwa-icons/KKMC.png" alt="로그인 페이지 로고" width={200} height={150} style={{ display: 'block', margin: '0 auto' }} />
+        <div style={{ backgroundColor: 'hsla(43, 30%, 95%, 1)', padding: '20px', width: '100%', boxSizing: 'border-box' }}>
+          <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
+            <input
+              type="email"
+              id="email"
+              placeholder="아이디"
+              required
+              style={{ marginBottom: '12px', padding: '8px', backgroundColor: 'hsla(0, 0%, 90%, 1)', width: '80%', border: '1px solid #d3d3d3', color: 'black' }}
+            />
+
+            <input
+              type="password"
+              id="password"
+              placeholder="비밀번호"
+              required
+              style={{ marginBottom: '20px', padding: '8px', backgroundColor: 'hsla(0, 0%, 90%, 1)', width: '80%', border: '1px solid #d3d3d3', color: 'black' }}
+            />
+            <button type="submit" style={{ padding: '10px', backgroundColor: '#000000', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer', width: '90%', marginBottom: '10px' }}>
+              로그인
+            </button>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '90%' }}>
+              <button type="button" style={{ textAlign: 'center', fontSize: '12px', padding: '10px', backgroundColor: 'hsla(43, 30%, 95%, 1)', color: 'black', border: 'none', borderRadius: '5px', cursor: 'pointer', width: '48%' }}>
+                비밀번호 찾기
+              </button>
+              <div style={{ width: '1px', height: '24px', backgroundColor: '#a7a7a7' }}></div>
+              <Link href="/flow/signup" style={{ textAlign: 'center', fontSize: '12px', padding: '10px', backgroundColor: 'hsla(43, 30%, 95%, 1)', color: 'black', border: 'none', borderRadius: '5px', cursor: 'pointer', width: '48%', display: 'block', textDecoration: 'none' }}>
+                회원가입
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
