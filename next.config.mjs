89301476992b6738e43d7271694129562dc793e9
DodoJBNU <dh723@jbnu.ac.kr>
2024-05-21 20:45:31 +0900

    vercel 이미지 기본 경로 변경

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import withPWA from "next-pwa";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
+  images: {
+    loader: "akamai",
+    path: "/public",
+  },
 };
 
 export default withPWA({
